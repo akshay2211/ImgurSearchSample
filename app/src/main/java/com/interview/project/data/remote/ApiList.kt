d0392f043177cad3dd62640d965ca93b118e5507
Akshay Sharma <fxn769@gmail.com>
2020-10-28 13:03:11 +0530

    Access key config in Constants.kt

--- app/src/main/java/com/interview/project/data/remote/ApiList.kt
+++ app/src/main/java/com/interview/project/data/remote/ApiList.kt
@@ -1,8 +1,10 @@
 package com.interview.project.data.remote
 
 import com.interview.project.model.BaseData
+import com.interview.project.ui.utils.Constants.IMGUR_API_KEY
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Path
 import retrofit2.http.Query
 
@@ -11,8 +13,9 @@ import retrofit2.http.Query
  * akshay2211@github.io
  */
 interface ApiList {
-    //https://api.imgur.com/3/gallery/search/1?q=vanilla
 
+    //https://api.imgur.com/3/gallery/search/1?q=vanilla
+    @Headers("Authorization:CLIENT-ID $IMGUR_API_KEY")
     @GET("gallery/search/{page}")
     suspend fun getSearchList(@Path("page") page: String, @Query("q") q: String): Response<BaseData>
 
--- app/src/main/java/com/interview/project/ui/utils/Constants.kt
+++ app/src/main/java/com/interview/project/ui/utils/Constants.kt
@@ -8,4 +8,7 @@ object Constants {
     var HeightPX = 0
     var WidthPX = 0
     var DENSITY = 0f
+
+    //Add Imgur Access key here
+    const val IMGUR_API_KEY = "<Access key>"
 }
